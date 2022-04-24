fbc2ca8b87c1928896d21926e4b5613cbf604298
leocassiosilva <leocassiosilva1234@gmail.com>
2022-04-24 18:27:25 -0300

    iniciando dasafio-19

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import {
-  BrowserRouter as Router,
+  BrowserRouter as Router, Link,
 } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <Router>
+
       <Navbar />
     </Router>
   );
